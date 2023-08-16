d85cc2ab26e1d8426b5986ca7330a550a0e34594
EJ Beron <ejberon@objectbright.com>
2023-08-16 09:06:33 +0800

    utilize useValueListenableHook

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:pokedex_app/src/providers/theme/dark_mode.dart';
+import 'package:pokedex_app/src/database/hive.dart';
+import 'package:pokedex_app/src/database/hives.dart';
 import 'package:pokedex_app/src/screens/home.dart';
 
 Future<void> main() async {
-  await Hive.initFlutter();
-  await Hive.openBox<bool>('darkModeBox');
+  await initHive();
 
   runApp(
     const ProviderScope(child: MyApp()),
   );
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final darkMode = ref.watch(darkModeProvider);
+    final box = useValueListenable(Hive.box<bool>(darkMode.box).listenable());
+    final isDark = box.get(darkMode.key, defaultValue: false) as bool;
+
     return MaterialApp(
       title: 'Pokedex App',
       theme: ThemeData(
         useMaterial3: true,
-        brightness: darkMode ? Brightness.dark : Brightness.light,
+        brightness: isDark ? Brightness.dark : Brightness.light,
       ),
       home: Scaffold(
         appBar: AppBar(
@@ -35,10 +38,8 @@ class MyApp extends ConsumerWidget {
             children: [
               const Text('Dark Mode'),
               Switch.adaptive(
-                value: darkMode,
-                onChanged: (value) {
-                  ref.read(darkModeProvider.notifier).toggle();
-                },
+                value: isDark,
+                onChanged: (value) => box.put(darkMode.key, value),
               ),
             ],
           ),
@@ -47,3 +48,43 @@ class MyApp extends ConsumerWidget {
     );
   }
 }
+
+/// Non-Hooks implementation of the Value Listenable Builder
+///
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ValueListenableBuilder(
+//       valueListenable: Hive.box<bool>(darkMode.box).listenable(),
+//       builder: (context, box, child) {
+//         final isDarkMode = box.get(darkMode.key, defaultValue: false) as bool;
+//         return MaterialApp(
+//           title: 'Pokedex App',
+//           theme: ThemeData(
+//             useMaterial3: true,
+//             brightness: isDarkMode ? Brightness.dark : Brightness.light,
+//           ),
+//           home: Scaffold(
+//             appBar: AppBar(
+//               title: const Text('Pokedex App'),
+//             ),
+//             body: const Home(),
+//             floatingActionButton: IntrinsicWidth(
+//               child: Row(
+//                 children: [
+//                   const Text('Dark Mode'),
+//                   Switch.adaptive(
+//                     value: isDarkMode,
+//                     onChanged: (value) => box.put(darkMode.key, value),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
