1c555807cc9dfd532ce63c01187e85d6bad8fec8
EJ Beron <ejberon@objectbright.com>
2023-08-18 15:13:34 +0800

    remove unused typedef

--- lib/src/hooks/infinite_scroller.dart
+++ lib/src/hooks/infinite_scroller.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
-typedef PageRequestListener<PageKeyType> = Future<void> Function(
-  PageKeyType pageKey,
-  PagingController pagingController,
-);
-
 PagingController<PageKeyType, ItemType>
     useInfiniteScroller<PageKeyType, ItemType>({
   required PageKeyType firstPageKey,
